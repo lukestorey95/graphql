6d5c55f585622aabf8959d758ea94800c95ace50
Luke Storey <lukemstorey@gmail.com>
2022-08-08 18:32:26 +0100

    Initial server setup with Mongo connection

---
+++ index.js
@@ -0,0 +1,23 @@
+const { ApolloServer } = require("apollo-server");
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+const MONGODB = process.env.MONGODB;
+
+const typeDefs = require("./graphql/typeDefs");
+const resolvers = require("./graphql/resolvers");
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+mongoose
+  .connect(MONGODB, { useNewUrlParser: true })
+  .then(() => {
+    console.log("Connected to mongodb");
+    return server.listen({ port: 6000 });
+  })
+  .then((res) => {
+    console.log(`server running at ${res.url}`);
+  });
