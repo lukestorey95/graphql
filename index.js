fe9bc51ebc2adb8c025643d654a3ab0240493925
Luke Storey <lukemstorey@gmail.com>
2022-08-09 21:32:22 +0100

    Add auth context

--- index.js
+++ index.js
@@ -1,15 +1,35 @@
 const { ApolloServer } = require("apollo-server");
 const mongoose = require("mongoose");
 require("dotenv").config();
+const jwt = require("jsonwebtoken");
 
 const MONGODB = process.env.MONGODB;
 
 const typeDefs = require("./graphql/typeDefs");
 const resolvers = require("./graphql/resolvers");
 
+const getUser = (token) => {
+  try {
+    if (token) {
+      return jwt.verify(token, process.env.PRIVATE_KEY);
+    }
+    return null;
+  } catch (err) {
+    return null;
+  }
+};
+
 const server = new ApolloServer({
   typeDefs,
   resolvers,
+  csrfPrevention: true,
+  cache: "bounded",
+  context: ({ req }) => {
+    const tokenWithBearer = req.headers.authorization || "";
+    const token = tokenWithBearer.split(" ")[1];
+    const user = getUser(token);
+    return { user };
+  },
 });
 
 mongoose
